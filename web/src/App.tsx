dc660df2ef759fbc4c7d3b78355b25a8a886aa0b
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2020-08-15 21:24:43 -0300

    Web finalizado

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Routees from './routees';
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <Routees />
+  );
+}
+
+export default App;
